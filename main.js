200c6d582993a480dd2f61321856d06747deffed
m0n411111 <bjn.ant1@gmail.com>
2021-03-18 19:02:15 +0100

    updated js file

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-const numbers = document.querySelectorAll('[data-number]')
-const operators = document.querySelectorAll('[data-operator]')
-const clearBtn = document.querySelector('[data-clear]')
-const allClearBtn = document.querySelector('[data-all-clear]')
-const equals = document.querySelector('[data-equals]')
-const previousText = document.querySelector('[data-previous]')
-const currentText = document.querySelector('[data-current]')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const clearBtn = document.querySelector('.clear')
+const allClearBtn = document.querySelector('.all-clear')
+const equals = document.querySelector('.equals')
+const previousText = document.querySelector('.previous')
+const currentText = document.querySelector('.current')
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch(operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "x":
+            return multiply(a, b);
+        case "÷":
+            return divide(a, b);
+    }
+}
+
+function display() {
+
+}
+
