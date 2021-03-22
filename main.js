c1251f29de8a12282ef0aa48e3a7c4fcd44ea30b
m0n411111 <bjn.ant1@gmail.com>
2021-03-22 16:10:08 +0100

    finished js

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const operators = document.querySelectorAll('.operator')
 const clearBtn = document.querySelector('.clear')
 const allClearBtn = document.querySelector('.all-clear')
 const equals = document.querySelector('.equals')
-const previousText = document.querySelector('.previous')
-const currentText = document.querySelector('.current')
-const result = document.querySelector('.display')
+const decimal = document.querySelector('.decimal')
+const display = document.querySelector('.display')
 
 let firstOperand = ""
 let secondOperand = ""
@@ -34,61 +33,85 @@ function operate(operator, a, b) {
     switch (operator) {
         case '+':
             return add(a, b);
-            break;
         case '-':
             return subtract(a, b);
-            break;
         case 'x':
             return multiply(a, b);
-            break;
         case '÷':
             if (b === 0) {
                 return "Error";
             } else {
                 return divide(a, b);
             }
-            break;
     }
 }
 
 function appendNumber(number) {
+    if(display.textContent === '0' || clearDisplay)
+    resetDisplay();
+    display.textContent += number;
+}
 
+function appendDecimal() {
+    if(clearDisplay) resetDisplay();
+    if(display.textContent === '') display.textContent = '0';
+    if(display.textContent.includes('.')) return;
+    display.textContent += '.';
+}
+
+function resetDisplay() {
+    display.textContent = "";
+    clearDisplay = false;
 }
 
 function chooseOperator(operator) {
-
+    if(currentOperator !== null) evaluate();
+    firstOperand = display.textContent;
+    currentOperator = operator;
+    clearDisplay = true;
 }
 
 function clear() {
-
+   display.textContent = display.textContent.toString().slice(0, -1);
 }
 
 function allClear() {
-
+    display.textContent = '0';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null;
 }
 
 function evaluate() {
-
+    if(currentOperator === null || clearDisplay) return;
+    if(currentOperator === '÷' && display.textContent === '0') {
+        alert('Error');
+        allClear;
+        return;
+    }
+    secondOperand = display.textContent;
+    display.textContent = roundResult(operate(currentOperator, firstOperand, secondOperand));
+    currentOperator = null;
 }
 
-function display() {
-
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
 }
 
-numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        appendNumber(number.textContent)
-    })
-})
-
-operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        chooseOperator(operator.textContent)
-    })
-})
+numbers.forEach((button) => 
+   button.addEventListener('click', () =>
+        appendNumber(button.textContent))
+);
+    
+operators.forEach((button) => 
+    button.addEventListener('click', () =>
+        chooseOperator(button.textContent))
+);
 
 clearBtn.addEventListener('click', clear)
 
 allClearBtn.addEventListener('click', allClear)
 
 equals.addEventListener('click', evaluate)
+
+decimal.addEventListener('click', appendDecimal)
